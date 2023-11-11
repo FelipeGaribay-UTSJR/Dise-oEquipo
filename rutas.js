f3d2187749c3417f0da8123b6c658a2f29b55698
FelipeGaribay-UTSJR <felipegu.ti22@utsjr.edu.mx>
2023-11-11 00:06:35 -0600

    5.1

--- rutas.js
+++ rutas.js
@@ -1,11 +1,17 @@
-var ruta = require('express');
+const express = require('express');
+const path = require('path'); // Asegúrate de que esté incluido
+
+const app = express();
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
-  
-  app.get("/Destacados.html", (req, res) => {
+});
+
+app.get("/Destacados.html", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'Destacados.html'));
 });
 
-module.exports = ruta;
+var port = process.env.PORT || 10000;
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
